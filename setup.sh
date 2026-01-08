4a5bc3602d29d61d8ac4bd913915395ee499d39b
Carter Prince <carteraprince@gmail.com>
2026-01-07 22:51:16 -0500

    add git

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo pacman -S --noconfirm --needed git
 mkdir -p $HOME/.local/src/
 git clone https://github.com/carterprince/dotfiles $HOME/.local/src/dotfiles
 cd $HOME/.local/src/dotfiles
