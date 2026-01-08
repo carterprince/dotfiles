3701f5a753bf5611fbe52d509b27958bc89de350
Carter Prince <carteraprince@gmail.com>
2026-01-07 22:58:16 -0500

    make setup.sh more robust

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 sudo pacman -S --noconfirm --needed git
 mkdir -p $HOME/.local/src/
-git clone https://github.com/carterprince/dotfiles $HOME/.local/src/dotfiles
+git clone https://github.com/carterprince/dotfiles $HOME/.local/src/dotfiles || true
 cd $HOME/.local/src/dotfiles
+git pull
 ./install.sh
