9763aa6e1ab2573d0432ec68139dd366c2df09bf
Carter Prince <carteraprince@gmail.com>
2026-01-12 10:58:57 -0500

    add newseum to favorite apps

--- install.sh
+++ install.sh
@@ -76,7 +76,7 @@ git clone https://github.com/Karmenzind/monaco-nerd-fonts /tmp/monaco-nerd-fonts
 sudo cp -r /tmp/monaco-nerd-fonts/fonts/ /usr/share/fonts/monaco-nerd-fonts
 # sudo fc-cache -fv
 gsettings set org.gnome.desktop.interface monospace-font-name 'Monaco Nerd Font 11'
-gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'firefox.desktop', 'org.gnome.Ptyxis.desktop']"
+gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'firefox.desktop', 'org.gnome.Ptyxis.desktop', 'newseum.desktop']"
 
 # terminal
 gsettings set org.gnome.Ptyxis.Profile:/org/gnome/Ptyxis/Profiles/profile0/ palette 'Dark Pastel'
