d938ccc8b436221313d717be9a927b98987d752b
Carter Prince <carteraprince@gmail.com>
2026-05-28 19:12:54 -0400

    move to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm --needed git
-mkdir -p $HOME/.local/src/
-git clone https://github.com/carterprince/dotfiles $HOME/.local/src/dotfiles || true
-cd $HOME/.local/src/dotfiles
-git pull
-./install.sh
+./bin/ok ./install.ok
